4dfa2d26b856d8768e4c793526c833397f968683
rohanneps <rohanneps@gmail.com>
2020-07-08 22:33:48 +1000

    Updated file output of MILP vs BPS algo to include impurity

--- develop/time_complexity_report/main.py
+++ develop/time_complexity_report/main.py
@@ -56,10 +56,9 @@ def main(num_of_positive_tokens):
     initialize_proces()
     total_executions = get_total_number_of_executions()
 
-    mil_bps_df = pd.DataFrame(columns=GV.MILP_BPS_FILE_COLUMNS)
-
     for impurity in tqdm(GV.IMPURITY_ARRAY, desc='Total progress'):
         output_df = pd.DataFrame(columns=GV.OUTPUT_FILE_COLUMNS)
+        mil_bps_df = pd.DataFrame(columns=GV.MILP_BPS_FILE_COLUMNS)
 
         for array_size in tqdm(GV.ARRAY_SIZE_LIST, desc='Arrays processing with impurity = {}'.format(impurity)):
             # initial random probability based on number of positive tokens
@@ -73,34 +72,40 @@ def main(num_of_positive_tokens):
             # milp vs bps
             milp_bps_row = {}
             milp_bps_row['DATA_SIZE'] = array_size
-            milp_bps_row['IMPURITY'] = impurity
+
             for run in range(GV.NUM_OF_RUNS):
                 # probability with impurity
                 impurity_injection_probabilities = helpers.get_impure_probabilities(probabilities, impurity)
                 # computing time for milp ef1 score
                 mil_exec_time, milp_s, milp_e = get_milp_ef1_executiong_time_and_bounding_indices(
-                    impurity_injection_probabilities)
+                                                                                                impurity_injection_probabilities
+                                                                                                )
                 milp_ef1_time_duration_list.append(mil_exec_time)
                 # computing time for brute force ef1 score
                 brute_force_ef1_time_duration_list.append(
-                    get_brute_force_ef1_execution_time(impurity_injection_probabilities))
+                                                        get_brute_force_ef1_execution_time(impurity_injection_probabilities)
+                                                        )
                 # computing time for bps ef1 score
                 bps_exec_time, bps_start, bps_end = get_bps_ef1_execution_time_and_bounding_indices(
-                    impurity_injection_probabilities)
+                                                                                                    impurity_injection_probabilities
+                                                                                                    )
                 bps_ef1_time_duration_list.append(bps_exec_time)
 
-                milp_bps_row['MILP_EF1_SCORE'] = ef1_helpers.get_substring_ef1_score(impurity_injection_probabilities,
-                                                                                     (milp_s, milp_e + 1))
-                milp_bps_row['BPS_EF1_SCORE'] = ef1_helpers.get_substring_ef1_score(impurity_injection_probabilities,
-                                                                                    (bps_start, bps_end + 1))
+                milp_bps_row['MILP_EF1_SCORE'] = ef1_helpers.get_substring_ef1_score(
+                                                                                    impurity_injection_probabilities,
+                                                                                    (milp_s, milp_e + 1)
+                                                                                    )
+                milp_bps_row['BPS_EF1_SCORE'] = ef1_helpers.get_substring_ef1_score(
+                                                                                    impurity_injection_probabilities,
+                                                                                    (bps_start, bps_end + 1)
+                                                                                    )
                 mil_bps_df = mil_bps_df.append(milp_bps_row, ignore_index=True)
 
             # computing mean and confidence interval
-            milp_ef1_mean, milp_ef1_confidence_interval = helpers.mean_confidence_interval_95(
-                milp_ef1_time_duration_list)
-            brute_force_ef1_mean, brute_force_ef1_confidence_interval = helpers.mean_confidence_interval_95(
-                brute_force_ef1_time_duration_list)
+            milp_ef1_mean, milp_ef1_confidence_interval = helpers.mean_confidence_interval_95(milp_ef1_time_duration_list)
+            brute_force_ef1_mean, brute_force_ef1_confidence_interval = helpers.mean_confidence_interval_95(brute_force_ef1_time_duration_list)
             bps_ef1_mean, bps_ef1_confidence_interval = helpers.mean_confidence_interval_95(bps_ef1_time_duration_list)
+
 
             run_row_dict['DATA_SIZE'] = array_size
             run_row_dict['TIME_MILP'] = milp_ef1_mean
@@ -111,21 +116,24 @@ def main(num_of_positive_tokens):
             run_row_dict['TIME_BPS_CI'] = bps_ef1_confidence_interval
             output_df = output_df.append(run_row_dict, ignore_index=True)
 
-        # write to file for each impurity and given p
+        # write MILP vs Brute Force file for each impurity and given p
         output_df['DATA_SIZE'] = output_df['DATA_SIZE'].astype(int)
         output_df = output_df[GV.OUTPUT_FILE_COLUMNS]
         output_df.to_csv(
-            os.path.join(GV.OUTPUT_DIR, GV.OUTPUT_FILE_NAME.format(
-                num_of_positive_tokens,
-                impurity
-            )
+                        os.path.join(GV.OUTPUT_DIR, GV.OUTPUT_FILE_NAME.format(
+                            num_of_positive_tokens,
+                            impurity
+                        )
                          ), sep='\t', index=False)
 
-        mil_bps_df.sort_values(by=['DATA_SIZE', 'IMPURITY'], ascending=True, inplace=True)
+        # write MILP vs BPS file for each impurity and given p
+        mil_bps_df['DATA_SIZE'] = mil_bps_df['DATA_SIZE'].astype(int)
+        mil_bps_df.sort_values(by=['DATA_SIZE'], ascending=True, inplace=True)
         mil_bps_df.to_csv(
-            os.path.join(GV.OUTPUT_DIR, GV.MILP_BPS_FILE_NAME.format(
-                num_of_positive_tokens
-            )
+                        os.path.join(GV.OUTPUT_DIR, GV.MILP_BPS_FILE_NAME.format(
+                            num_of_positive_tokens,
+                            impurity
+                        )
                          ), sep='\t', index=False)
 
 
